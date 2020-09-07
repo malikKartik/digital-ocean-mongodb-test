ec4154792615d8205d8b165725889f188f579f9c
kartik malik <kartikforhack@gmail.com>
2020-09-07 17:53:10 +0530

    change of mogouri

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,2 +1,2 @@
-// exports.MONGO_URI = 'mongodb://127.0.0.1:27017/test-db'
-exports.MONGO_URI = 'mongodb+srv://malik:malik@businessideas-dhipt.mongodb.net/digital-ocean-test?retryWrites=true&w=majority'
+exports.MONGO_URI = 'mongodb://127.0.0.1:27017/test-db'
+// exports.MONGO_URI = 'mongodb+srv://malik:malik@businessideas-dhipt.mongodb.net/digital-ocean-test?retryWrites=true&w=majority'
